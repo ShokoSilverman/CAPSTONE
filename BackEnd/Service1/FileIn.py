02f6323400e2d8efdff6718cd32300975bb5353b
Simon Silverman <ssilverman@student.neumont.edu>
2022-10-04 16:37:37 -0600

    finished service 1

--- BackEnd/Service1/FileIn.py
+++ BackEnd/Service1/FileIn.py
@@ -1,18 +1,26 @@
 from flask import Flask, redirect, url_for, request
 import pandas as pd
-# import numpy
 
-
-# app = Flask(__name__)
+app = Flask(__name__)
 
 #clean up any null values in the csv (replace or delete them) (deleting them for now, will come back to replace with means/medians)
-def remove_nulls(file) -> pd.DataFrame:
+#should return a valid and clean json version of the dataframe
+def remove_nulls(file) -> dict:
     df: pd.DataFrame = pd.read_csv(file)
-    df = df.fillna(df.mean()) #numbers are a lot easier to fill in with the mean
+    df = df.fillna(df.mean(numeric_only=True)) #numbers are a lot easier to fill in with the mean
     df = df.dropna() #clear out any fields that still have nulls in them
-    print(df.head(20))
+    # print(df.head(20))
+    return df.to_json()
 
 
-remove_nulls(r'testData\Data_With_Nulls_Age.csv')
+#TODO flask endpoint here, make sure to use pd.to_csv to return the csv file
+@app.route('/cleanCSV', methods=['POST'])
+def clean_data():
+    file_name: str = request.files["dirty_csv"].filename
+    dirty_csv = request.files["dirty_csv"]
+    clean_dict = {file_name : remove_nulls(dirty_csv)}
+    print(type(clean_dict[file_name]))
+    return clean_dict
 
-# app.run(host='0.0.0.0', port=8080)
+
+app.run(host='0.0.0.0', port=8080)
